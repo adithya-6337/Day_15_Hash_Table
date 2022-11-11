0f8b86f2d0b539d85aa98237a8dc0304219a5874
adithya-6337 <veldandiadithya337@gmail.com>
2022-11-11 11:45:15 +0530

    Add : find frequency of words

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String paragraph = "A cat is sleeping under the bed";
+
+        String[] splitArray = paragraph.split(" ");
+        MyHashTable<String, Integer> hashTable = new MyHashTable<>();
+        for (String word : splitArray) {
+            if (hashTable.containsKey(word)) {
+                int count = hashTable.get(word);
+                hashTable.replace(word, count + 1);
+            } else {
+                hashTable.put(word, 1);
+            }
+        }
+        hashTable.print();
+    }
+}
